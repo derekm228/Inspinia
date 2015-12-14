b165bdb00bcaebc3b356acbf373b64c853b0be1f
dmiller@Advent.com <derekm228@gmail.com>
2015-12-14 14:18:22 -0500

    main_12 and main_13
    main_12.html:
    1. screens list up top
    2. hover animation on screen links
    3. hover enables sub-pages to display and changes the background color
    to a lighter-black
    4. search bar top-right
    5. screen title and breadcrumb nav underneath screens list
    6. screen body underneath title/breadcrumb
    
    main_13.html:
    1. pretty much the same as main_12
    2. screen body segmented into tiles
    3. left tile only shows research tree and finder
    4. middle tile shows note list, filtering options, and keyword search
    5. right tile is note preview
    6. gray background to show the components are tiles

--- Static_Full_Version/ckeditor/config.js
+++ Static_Full_Version/ckeditor/config.js
@@ -10,35 +10,24 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
 		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		'/',
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
 		{ name: 'styles', groups: [ 'styles' ] },
 		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
 		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	config.removeButtons = 'Image,Source,About';
+	config.removeButtons = 'Source,Save,NewPage,Preview,Print,Templates,SelectAll,Find,Form,Checkbox,Radio,TextField,Textarea,Select,Button,HiddenField,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,ImageButton,Flash,Smiley,PageBreak,Iframe,ShowBlocks,About';
 
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
-
-	// Custom Tamale code
-	// Remove MagicLine plugin
-	config.removePlugins = 'magicline';
-
-	// Add image upload
-	config.extraPlugins = 'image2, uploadimage, uploadwidget, filetools, notificationaggregator, notification, lineutils, widget';
-	config.extraPlugins = 'uploadwidget';
+	// Remove MagicLine and ElementsPath plugins
+	config.removePlugins = 'magicline, elementspath';
 };
